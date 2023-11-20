40a0a0242dc84116e82f68962d80e7bb96f29029
David Diaz <jd_015@hotmail.com>
2023-11-19 20:54:11 -0500

    Add my name in config.py

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "Banckchurn API de Jeison David Díaz Espitia"
 
     class Config:
         case_sensitive = True
